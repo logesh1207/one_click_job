0bb30379463449d2439a9b71adcfb9230fc54572
logesh1207 <141866793+logesh1207@users.noreply.github.com>
2024-08-05 12:07:14 +0530

    Create react-country-state-city.d.ts

---
+++ src/types/react-country-state-city.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-country-state-city' {
+  export const Country: React.FC<any>;
+  export const State: React.FC<any>;
+  export const City: React.FC<any>;
+  export const CountrySelect: React.FC<any>;
+  export const StateSelect: React.FC<any>;
+  export const CitySelect: React.FC<any>;
+}
