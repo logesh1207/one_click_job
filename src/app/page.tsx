731ec6c4781fd8b671de0d17571a7bfe542a1c14
logesh1207 <141866793+logesh1207@users.noreply.github.com>
2024-08-05 13:24:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,28 @@ import { addOrgAndUserData, JobModel } from "@/models/Job";
 import Hero from "./components/hero";
 import Jobs from "./components/jobs";
 import { getUser } from "@workos-inc/authkit-nextjs";
+//import { useState } from "react";
+import mongoose from 'mongoose';
 
-
+mongoose.connect(process.env.MONGO_URI as string,{
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, 
+  socketTimeoutMS: 45000,
+});
 
 export default async function Home() {
+  //const[latestJobs,setLatestJobs]=useState([]);
+  //const[error,serError]=useState(null);
+  //useEffect(()=>{
+  try{
   const {user}=await getUser();
   const latestJobs=await addOrgAndUserData(
-    await JobModel.find({},{},{limit:5,sort:'-createdAt'}),
+    await JobModel.find({},{},{limit:3,sort:'-createdAt'}).lean(),
     user,
   );
+ 
+      
   return (
     <>
 
@@ -18,4 +31,9 @@ export default async function Home() {
    <Jobs  header={''} jobs={latestJobs}/>
    </>
   );
+}
+  catch (error) {
+    console.error('Error in Home component:', error);
+    return <div>Error loading jobs</div>;
+  }
 }
